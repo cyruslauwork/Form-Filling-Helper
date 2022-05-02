99a459999cb922d0d2abbd8df2c9274c1364894a
Cyrus Lau <cyruslau.work@gmail.com>
2022-05-02 17:55:47 +0800

    Initial Commit

---
+++ Form Filling Helper/Form_Filling_HelperApp.swift
@@ -0,0 +1,17 @@
+//
+//  Form_Filling_HelperApp.swift
+//  Form Filling Helper
+//
+//  Created by Cyrus on 2/5/2022.
+//
+
+import SwiftUI
+
+@main
+struct Form_Filling_HelperApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
